15c83caad7d7304f23f10751e38738a8fcaf0354
Naren <deadlyassassin2003@gmail.com>
2025-04-03 15:13:56 +0530

    Adding Auth to the Item Route

--- router/Item/itemRoute.js
+++ router/Item/itemRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { addItem, getAllItems, updateItemQuantity, deleteItem,} from "../../controller/itemController.js";
+import {verifyAdmin} from "../../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", addItem);         
-router.get("/", getAllItems);       
-router.put("/:id", updateItemQuantity); 
-router.delete("/:id", deleteItem);
+router.post("/",verifyAdmin, addItem);         
+router.get("/",verifyAdmin, getAllItems);       
+router.put("/:id",verifyAdmin, updateItemQuantity); 
+router.delete("/:id",verifyAdmin, deleteItem);
 
 export default router;
